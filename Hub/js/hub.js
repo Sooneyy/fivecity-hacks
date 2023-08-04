77c7a6e81b72f9eb5620584d15fe446fc83d7981
Sooney <111024291+Sooneyy@users.noreply.github.com>
2023-08-05 00:26:33 +0200

    Update hub.js

--- Hub/js/hub.js
+++ Hub/js/hub.js
@@ -1,9 +1,9 @@
-const hacksSection = document.querySelector('.cards-list');
+const $hacksSection = $(".heists-list");
 
 const heistsList = [
     {
         heist: 'Vangelico',
-        colorNum: '60' // Numer hue
+        isNew: 1,
     },
 ]
 
@@ -30,59 +30,58 @@ const hacksList = [
     },
 ]
 
-window.onload = () => {
-    alert("ALGORYTM NA SEJF ZOSTAŁ NAPRAWIONY. TERAZ ZAWSZE DA SIĘ PRZEJŚĆ! Czas na hacka do drzwi też został zmieniony (zmniejszony) na taki jak w grze 😉")
-}
-
 for (let i = 0; i < heistsList.length; i++) {
 
-    const card = document.createElement('div');
-    const cTitle = document.createElement('div');
-    const hsName = document.createElement('span');
+    const $heist = $("<div>");
+    const $hsName = $("<div>");
+    const $hsItems = $("<div>");
 
     if (i >= 4) {
-        hacksSection.style.justifyContent = 'flex-start';
-        hacksSection.style.overflowX = 'auto';
-        hacksSection.addEventListener('wheel', (e) => horizontalScroll(e));
+        $hacksSection.css({
+            "justify-content": "flex-start",
+            "overflow-x": "auto"
+        });
+
+        $hacksSection.on("wheel", function (e) {
+            horizontalScroll(e);
+        });
     }
 
-    card.classList.add('card');
-    hacksSection.appendChild(card);
+    $heist.addClass("heist");
+    $hacksSection.append($heist);
 
-    cTitle.textContent = 'Heist ';
-    hsName.textContent = heistsList[i].heist;
-    hsName.classList.add('heist-name');
-    hsName.style.color = `hsl(${heistsList[i].colorNum}, 100%, 35%)`;
-    hsName.style.filter = `drop-shadow(0 0 8px hsl(${heistsList[i].colorNum}, 100%, 35%))`
-    cTitle.appendChild(hsName);
-    cTitle.classList.add('card-title');
-    card.appendChild(cTitle);
+    if(heistsList[i].isNew){
+        $hsName.text(`new: Heist - ${heistsList[i].heist}`);
+    }else{
+        $hsName.text(`Heist - ${heistsList[i].heist}`);
+    }
+    $hsName.addClass("heist-title");
+    $heist.append($hsName);
+
+    $hsItems.addClass("heist-items");
+    $heist.append($hsItems);
 
     for (let j = 0; j < hacksList.length; j++) {
 
         if (hacksList[j].heist !== heistsList[i].heist) continue;
 
-        const cItems = document.createElement('div');
-        const button = document.createElement('button');
-        const hcName = document.createElement('span');
-        const link = document.createElement('a');
+        const $link = $("<a>")
+        const $button = $("<button>");
+        const $hcCat = $("<div>");
+        const $hcName = $("<div>");
 
-        cItems.classList.add('card-items');
-        card.appendChild(cItems);
+        $link.attr("href", hacksList[j].link);
+        $hsItems.append($link)
 
-        cItems.appendChild(button);
+        $link.append($button);
 
-        hcName.textContent = hacksList[j].name;
-        hcName.classList.add('hack-name');
-        hcName.style.color = `hsl(${heistsList[i].colorNum}, 100%, 35%)`;
-        button.appendChild(hcName);
+        $hcCat.text(hacksList[j].heist);
+        $hcCat.addClass("heist-name");
+        $button.append($hcCat);
 
-        link.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="9"/><path d="m11 15l3-3l-3-3"/></g></svg>`;
-        link.title = 'Przejdź do hacku - ' + hacksList[j].name;
-        link.setAttribute('href', hacksList[j].link);
-        link.style.borderColor = `hsl(${heistsList[i].colorNum}, 100%, 60%)`;
-        link.style.color = `hsl(${heistsList[i].colorNum}, 100%, 60%)`;
-        button.appendChild(link);
+        $hcName.text(hacksList[j].name);
+        $hcName.addClass("hack-name");
+        $button.append($hcName);
     }
 }
 
@@ -90,3 +89,25 @@ function horizontalScroll(x) {
     hacksSection.scrollLeft += (x.deltaY);
 }
 
+function copyCode() {
+    var $temp = $("<input>");
+    $("body").append($temp);
+
+    $temp.val("sooney").select();
+
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+$(document).ready(function () {
+
+    $("#discord").on("click", function () {
+        $("#copy").show();
+
+        setTimeout(() => {
+            $("#copy").hide();
+        }, 500)
+
+        copyCode();
+    })
+})
