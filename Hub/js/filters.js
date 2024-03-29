8ea7e7af16d5d4649942b59590a3b881d59ed8b9
Sooneyy <hallowreapeer84@gmail.com>
2024-03-29 02:37:02 +0100

    njnjn

--- Hub/js/filters.js
+++ Hub/js/filters.js
@@ -1,79 +1,44 @@
 import { createList, hacksList } from "./hub.js";
 
-const input = document.querySelector(".search > input[type='text']");
+const input = document.querySelector(".search-input");
 const dropdownHeader = document.querySelector(".dropdown-header");
-const dropdownArrow = document.querySelector(".dropdown-header > svg");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
 dropdownHeader.addEventListener("click", () => {
-    dropdownMenu.classList.toggle("opened");
+    dropdownMenu.classList.toggle("open");
+    dropdownHeader.classList.toggle("open");
 });
 
-input.addEventListener("keyup", searchFilter);
+window.addEventListener("keyup", filterByInput);
 
-const heistFilters = [
-    {
-        text: "Vangelico",
-        date: new Date("2023-07-23T19:00:00"),
-        latest: 0
-    },
-    {
-        text: "Willa",
-        date: new Date("2023-09-09T19:00:00"),
-        latest: 0
-    },
-    {
-        text: "Fleeca",
-        date: new Date("2023-11-06T19:00:00"),
-        latest: 0
-    },
-    {
-        text: "Car Dealer",
-        date: new Date("2023-11-19T19:00:00"),
-        latest: 0
-    },
-    {
-        text: "SGOC",
-        date: "",
-        latest: 1
-    },
-    {
-        text: "Wszystkie",
-        latest: 0
-    }
-]
-
-document.getElementById("count").textContent = `${heistFilters.length - 1} / ${hacksList.length}`;
+const heistFilters = ["Vangelico", "Willa", "Fleeca", "Car Dealer", "SGOC", "Wszystkie"];
 
 for(let i = 0; i < heistFilters.length; i++) {
     const heistOption = document.createElement("div");
 
-    heistOption.innerHTML = heistFilters[i].text;
-    heistOption.className = "heist-option";
+    heistOption.textContent = heistFilters[i];
+    heistOption.className = "filter";
     dropdownMenu.appendChild(heistOption);
-
-    if(heistFilters[i].latest){
-        document.querySelector(".last-heist > b").textContent = heistFilters[i].text;
-        document.querySelector("#timestamp").textContent =  "iles czas temu";
-    }
 }
 
-const filterOptions = document.querySelectorAll(".heist-option");
-const filterOption = document.querySelector(".filter-heist-name");
+const filters = document.querySelectorAll(".filter");
+const selectedFilter = document.querySelector(".filter-name");
 
-filterOptions.forEach((item) => {
+filters.forEach((item) => {
     item.addEventListener("click", () => {
-        filterOption.textContent = item.textContent === "Wszystkie" ? "Heist" : item.textContent;
-        dropdownMenu.classList.toggle("opened");
-        dropdownFilter();
+        selectedFilter.textContent = item.textContent === "Wszystkie" ? "Heist" : item.textContent;
+        dropdownHeader.classList.remove("open");
+        dropdownMenu.classList.remove("open");
+        filterByDropdown();
     })
 })
 
 var selectedHeist = hacksList;
 
-function dropdownFilter(){
+function filterByDropdown(){
+    let filter = selectedFilter.textContent.toLowerCase();
+
     selectedHeist = hacksList;
-    let filter = filterOption.textContent.toLowerCase();
     
     if(filter !== "heist"){
         selectedHeist = selectedHeist.filter((hack) => filter === hack.heist.toLowerCase());
@@ -84,13 +49,12 @@ function dropdownFilter(){
     }
 }
 
-function searchFilter(){
+function filterByInput(){
     let listArr = selectedHeist;
 
     if(input.value.trim().length > 0){
         listArr = listArr.filter((hack) => 
-            hack.name.toLowerCase().includes(input.value.trim().toLowerCase()) || 
-            hack.minigame.toLowerCase().includes(input.value.trim().toLowerCase()) || 
+            hack.desc.toLowerCase().includes(input.value.trim().toLowerCase()) || 
             hack.heist.toLowerCase().includes(input.value.trim().toLowerCase())
         );
     }
--- Hub/js/hub.js
+++ Hub/js/hub.js
@@ -1,123 +1,118 @@
-const heistsList = document.querySelector(".heists-list");
+const heists = document.querySelector(".heists");
 
 export const hacksList = [
     {
         heist: 'Vangelico',
-        name: 'Przełączanie zasilania',
-        link: './vangelico_switch'
+        desc: 'Przełączanie zasilania',
+        link: './vangelico_switch',
     },
     {
         heist: 'Vangelico',
-        name: 'Wyłączanie zasilania',
-        link: './vangelico_power'
+        desc: 'Wyłączanie zasilania',
+        link: './vangelico_power',
     },
     {
         heist: 'Vangelico',
-        name: 'Włamywanie do drzwi',
-        link: './vangelico_doors'
+        desc: 'Włamywanie do drzwi',
+        link: './vangelico_doors',
     },
     {
         heist: 'Vangelico',
-        name: 'Włamywanie do sejfu',
-        link: './vangelico_safe'
+        desc: 'Włamywanie do sejfu',
+        link: './vangelico_safe',
     },
     {
         heist: 'Willa',
-        name: 'Wyłączanie zabezpieczenia',
-        link: './willa_serv'
+        desc: 'Wyłączanie zabezpieczenia',
+        link: './willa_serv',
     },
     {
         heist: 'Willa',
-        name: 'Otwieranie drzwi do winiarni',
-        link: './willa_winedoors'
+        desc: 'Otwieranie drzwi do winiarni',
+        link: './willa_winedoors',
     },
     {
         heist: 'Willa',
-        name: 'Włamywanie do małego sejfu',
-        link: './willa_minisafe'
+        desc: 'Włamywanie do małego sejfu',
+        link: './willa_minisafe',
     },
     {
         heist: 'Willa',
-        name: 'Włamywanie do głównego sejfu',
-        link: './willa_safe'
+        desc: 'Włamywanie do głównego sejfu',
+        link: './willa_safe',
     },
     {
         heist: 'Fleeca',
-        name: 'Wyłączanie zabezpieczeń',
-        link: './fleeca_box'
+        desc: 'Wyłączanie zabezpieczeń',
+        link: './fleeca_box',
     },
     {
         heist: 'Fleeca',
-        name: 'Otwieranie drzwi do sejfu',
-        link: './fleeca_doors1'
+        desc: 'Otwieranie drzwi do sejfu',
+        link: './fleeca_doors1',
     },
     {
         heist: 'Fleeca',
-        name: 'Otwieranie głównych drzwi sejfu',
-        link: './fleeca_panel'
+        desc: 'Otwieranie głównych drzwi sejfu',
+        link: './fleeca_panel',
     },
     {
         heist: 'Fleeca',
-        name: 'Otwieranie drugich drzwi do sejfu',
-        link: './fleeca_doors2'
+        desc: 'Otwieranie drugich drzwi do sejfu',
+        link: './fleeca_doors2',
     },
     {
         heist: 'Car Dealer',
-        name: 'Zdejmowanie nadajnika',
-        link: './cardealer_gps'
+        desc: 'Zdejmowanie nadajnika',
+        link: './cardealer_gps',
     },
     {
         heist: 'SGOC',
-        name: 'Wyłączanie kamer',
-        link: './sgoc_cameras'
+        desc: 'Wyłączanie kamer',
+        link: './sgoc_cameras',
     },
     {
         heist: 'SGOC',
-        name: 'Wyłaczanie PC',
-        link: './sgoc_pc'
+        desc: 'Wyłaczanie PC',
+        link: './sgoc_pc',
     },
     {
         heist: 'SGOC',
-        name: 'Wyłączanie prądu',
-        link: './sgoc_box'
+        desc: 'Wyłączanie prądu',
+        link: './sgoc_box',
     },
 ]
 
-window.onload = () => {
-    document.querySelector(".boosting-container").style.display = "none";
-}
-
 export function createList(array){
-    clearList();
+    heists.innerHTML = "";
 
     for(let i = 0; i < array.length; i++) {
         const heist = document.createElement("div");
-        const heistName = document.createElement("div");
-        const hackName = document.createElement("div");
+        const heistName = document.createElement("p");
+        const hackDesc = document.createElement("p");
         const linkButton = document.createElement("button");
-        const favouriteButton = document.createElement("button");
     
-        heist.classList.add("heist");
-        heistsList.appendChild(heist);
+        heist.classList.add("heist-wrapper");
+        heists.appendChild(heist);
     
         heistName.innerHTML = `Heist: <b>${array[i].heist}</b>`;
-        heistName.classList.add("heist-name");
+        heistName.classList.add("name");
         heist.appendChild(heistName);
     
-        hackName.innerHTML = `Hack: <b>${array[i].name}</b>`;
-        hackName.classList.add("hack-name");
-        heist.appendChild(hackName);
+        hackDesc.textContent = `${array[i].desc}`;
+        hackDesc.classList.add("desc");
+        heist.appendChild(hackDesc);
     
-        linkButton.textContent = "Przejdź";
-        linkButton.dataset.link = array[i].link;
-        heist.appendChild(linkButton);
-
-        linkButton.textContent = "Przejdź";
+        linkButton.innerHTML = `Przejdź 
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <g fill="none"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="m14.707 5.636l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-1.414-1.414l3.95-3.95H4a1 1 0 1 1 0-2h13.243l-3.95-3.95a1 1 0 1 1 1.414-1.414"/>
+                </g>
+            </svg>`;
         linkButton.dataset.link = array[i].link;
         heist.appendChild(linkButton);
     }
 
-    const button = document.querySelectorAll(".heist > button");
+    const button = document.querySelectorAll(".heist-wrapper > button");
 
     button.forEach((button) => {
         const link = button.dataset.link;
@@ -126,66 +121,7 @@ export function createList(array){
             window.open(link, "_self");
         })
     })
-
-    const appear = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if(entry.isIntersecting){
-                entry.target.classList.add("appear");
-            }else{
-                entry.target.classList.remove("appear");
-            }
-        })
-    })
-    
-    const items = document.querySelectorAll(".heist");
-    
-    items.forEach((item) => {
-        appear.observe(item);
-    })
 }
+
 createList(hacksList);
 
-function clearList(){
-    heistsList.querySelectorAll(".heist").forEach((item) => item.remove());
-}
-
-document.querySelector(".search > input[type='text']").addEventListener("focus", () => {
-    document.querySelector(".search").classList.add("focus");
-})
-
-document.querySelector(".search > input[type='text']").addEventListener("blur", () => {
-    document.querySelector(".search").classList.remove("focus");
-})
-
-const boostingList = [
-    {
-        name: "Znajdowanie danego przedziału po wybranych znakach",
-        link: "./boosting_chars"
-    },
-]
-
-for(let x = 0; x < boostingList.length; x++){
-    const el1 = document.createElement("div");
-    const el2 = document.createElement("div");
-    const bt = document.createElement("button");
-
-    el1.classList.add("boosting");
-    document.querySelector(".boosting-list").appendChild(el1);
-
-    el2.textContent = boostingList[x].name;
-    el1.appendChild(el2);
-
-    bt.textContent = "Wybierz";
-    bt.dataset.link = boostingList[x].link;
-    el1.appendChild(bt);
-}
-
-const button = document.querySelectorAll(".boosting > button");
-
-button.forEach((button) => {
-    const link = button.dataset.link;
-
-    button.addEventListener("click", () => {
-        window.open(link, "_self");
-    })
-})
